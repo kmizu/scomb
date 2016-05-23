6b457e4fa47bc4cb0df35f562c3104ea41cf2f2b
Kota Mizushima <mizukota@gmail.com>
2016-05-23 15:16:16 +0900

    shorter code

--- src/main/scala/com/github/kmizu/scomb/Calculator.scala
+++ src/main/scala/com/github/kmizu/scomb/Calculator.scala
@@ -1,14 +1,14 @@
 package com.github.kmizu.scomb
 
 object Calculator extends SComb {
-  def expression: Parser[Int] = additive
-  def additive: Parser[Int] = (multitive ~ (string("+") ~ multitive | string("-") ~ multitive).*).map{
+  def expression: Parser[Int] = add
+  def add: Parser[Int] = (mult ~ (string("+") ~ mult | string("-") ~ mult).*).map{
     case (l, rs) => rs.foldLeft(l) { case (e, ("+", r)) => e * r; case (e, ("-", r)) => e / r }
   }
-  def multitive: Parser[Int] = (primary ~ (string("*") ~ primary | string("/") ~ primary).*).map{
+  def mult: Parser[Int] = (prm ~ (string("*") ~ prm | string("/") ~ prm).*).map{
     case (l, rs) => rs.foldLeft(l) { case (e, ("*", r)) => e * r; case (e, ("/", r)) => e / r }
   }
-  def primary: Parser[Int] = (for {
+  def prm: Parser[Int] = (for {
     _ <- string("("); e <- expression; _ <- string(")") } yield e) | number
   def number: Parser[Int] = oneOf('0'to'9').*.map{digits => digits.mkString.toInt}
 
